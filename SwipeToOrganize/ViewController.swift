93985afc8f5bdbf3956c92b694e9fa96049661d8
Alexander Lester <alexander480@gmail.com>
2020-04-26 00:40:50 -0400

    Initial Commit

---
+++ SwipeToOrganize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeToOrganize
+//
+//  Created by Alexander Lester on 4/26/20.
+//  Copyright © 2020 Alexander Lester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
